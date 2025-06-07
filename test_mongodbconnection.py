26c72597eb6206353e258e950673a46786a7377a
Aditya Pratap <pratapaditya366@gmail.com>
2025-06-07 10:22:25 +0530

    mongodbsetup

--- test_mongodbconnection.py
+++ test_mongodbconnection.py
@@ -1,7 +1,6 @@
-
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://krishaitechnologies:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://<username>:<password>@cluster0.fv0ssf1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
